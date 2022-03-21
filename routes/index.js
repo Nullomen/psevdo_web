ee8dd37deb4890e3d3df9a39e095fd6a47cc9a36
shapovalovand <shapovalov.exceedteam@gmail.com>
2022-03-21 18:47:55 +0300

    feat: dublicate project

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/admin', function(req, res, next) {
+  res.render('admin', { title: 'Express' });
+});
+router.get('/visitor', function(req, res, next) {
+  res.render('visitor', { title: 'Express' });
+});
+router.get('/TeamsMain', function(req, res, next) {
+  res.render('TeamsMain', { title: 'Express' });
+});
+router.get('/TeamDetail', function(req, res, next) {
+  res.render('TeamDetail', { title: 'Express' });
+});
+router.get('/AdminLogin', function(req, res, next) {
+  res.render('AdminLogin', { title: 'Express' });
+});
+module.exports = router;
